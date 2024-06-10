4180a3917411e0dc41888551630b60bf85abf92c
sodiq <abdulazeezsodiq403@gmail.com>
2024-06-10 03:27:50 +0100

    i fixed this again...

--- app/quiz/detial.tsx
+++ app/quiz/detial.tsx
@@ -36,8 +36,8 @@ export const QuizDetial = () => {
 
     const subjects = ['Mathematics', 'English', 'Physics', 'Chemistry', 'Commerce', 'Biology', 'Accounting', 'English Literature'];
     const years = [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001];
-    const [selectedSubject, setSelectedSubject] = useState("English");
-    const [selectedYear, setSelectedYear] = useState("2019");
+    const [selectedSubject, setSelectedSubject] = useState<any>("English");
+    const [selectedYear, setSelectedYear] = useState<any>("2019");
 
     const [questions, setQuestions] = useState([]);
     const [open, setOpen] = useState(false);
@@ -153,7 +153,7 @@ export const QuizDetial = () => {
                         </SelectTrigger>
                         <SelectContent>
 
-                            {years.map((year) => (
+                            {years.map((year: any) => (
                                 <SelectItem key={year} value={year}>
                                     {year}
                                 </SelectItem>
